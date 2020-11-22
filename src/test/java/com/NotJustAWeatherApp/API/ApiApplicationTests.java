f0e775b8db0d5be996ebe5e574fa6035eaf54649
ChaseBosman <54570221+ChaseBosman@users.noreply.github.com>
2020-11-22 13:12:54 -0800

    Created Spring project

---
+++ src/test/java/com/NotJustAWeatherApp/API/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NotJustAWeatherApp.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
