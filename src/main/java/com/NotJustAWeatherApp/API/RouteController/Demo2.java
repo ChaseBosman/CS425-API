5997c46c89ad0a4cd58459f1d7697988d2a20dfe
ChaseBosman <54570221+ChaseBosman@users.noreply.github.com>
2020-11-29 23:41:58 -0800

    Implemented single location and multiple location weather lookup

--- src/main/java/com/NotJustAWeatherApp/API/RouteController/Demo2.java
+++ src/main/java/com/NotJustAWeatherApp/API/RouteController/Demo2.java
@@ -1,7 +1,15 @@
 package com.NotJustAWeatherApp.API.RouteController;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.*;
+import org.apache.commons.logging.Log;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 // mark this class as a com.NotJustAWeatherApp.api.api.controller to handle /demo requests
 
 @RestController
@@ -9,5 +17,58 @@ import org.springframework.web.bind.annotation.RestController;
 public class Demo2 {    // create GET endpoint to serve demo data at /demo/data
     @GetMapping(value = "/multiple")
     public String getDemoData() {
-        return "Its sunny the entire way!";
-    }}
+
+        //Define list to get all latlng for the route
+        List<LatLng> path = new ArrayList();
+
+        //Execute Directions API request
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey("AIzaSyAsxeRqO3WL308LSQdWPD1eOAUNmEw2_QA")
+                .build();
+        DirectionsApiRequest req = DirectionsApi.getDirections(context, "Brentwood,CA", "Antioch,CA");
+        try {
+            DirectionsResult res = req.await();
+
+            //Loop through legs and steps to get encoded polylines of each step
+            if (res.routes != null && res.routes.length > 0) {
+                DirectionsRoute route = res.routes[0];
+
+                if (route.legs != null) {
+                    for (int i = 0; i < route.legs.length; i++) {
+                        DirectionsLeg leg = route.legs[i];
+                        if (leg.steps != null) {
+                            for (int j = 0; j < leg.steps.length; j++) {
+                                DirectionsStep step = leg.steps[j];
+                                if (step.steps != null && step.steps.length > 0) {
+                                    for (int k = 0; k < step.steps.length; k++) {
+                                        DirectionsStep step1 = step.steps[k];
+                                        EncodedPolyline points1 = step1.polyline;
+                                        if (points1 != null) {
+                                            //Decode polyline and add points to list of route coordinates
+                                            List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
+                                            for (com.google.maps.model.LatLng coord1 : coords1) {
+                                                path.add(new LatLng(coord1.lat, coord1.lng));
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    EncodedPolyline points = step.polyline;
+                                    if (points != null) {
+                                        //Decode polyline and add points to list of route coordinates
+                                        List<com.google.maps.model.LatLng> coords = points.decodePath();
+                                        for (com.google.maps.model.LatLng coord : coords) {
+                                            path.add(new LatLng(coord.lat, coord.lng));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            return ex.getLocalizedMessage();
+        }
+        return "Its sunny the entire way!" + path;
+    }
+}
--- src/main/java/com/NotJustAWeatherApp/API/SingleLocationController/Demo.java
+++ src/main/java/com/NotJustAWeatherApp/API/SingleLocationController/Demo.java
@@ -1,4 +1,7 @@
 package com.NotJustAWeatherApp.API.SingleLocationController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +18,15 @@ public class Demo
     private RestTemplate restTemplate;
 
     @GetMapping(value = "/single")
-    public String getDemoData() {
-        final String uri = "https://gturnquist-quoters.cfapps.io/api/random";
+    public String getDemoData() throws JsonProcessingException {
+        final String pointsURI = "https://api.weather.gov/points/39.7456,-97.0892";
 
-        Quote quote = restTemplate.getForObject(uri, Quote.class);
-
-        assert quote != null;
-        return quote.getValue().getQuote();
+        JsonNode pointProperties = restTemplate.getForObject(pointsURI, JsonNode.class);
+        String gridURI = pointProperties.get("properties").get("forecastGridData").toString();
+        gridURI = gridURI.replace("\"", "");
+        
+        JsonNode gridProperties = restTemplate.getForObject(gridURI, JsonNode.class);
+        
+        return gridProperties.at("/properties/maxTemperature/values").toPrettyString();
     }
 }
