387072653a7629279faebe491c37bc9c2d97002a
cole <coletknsn@gmail.com>
2020-12-07 16:47:54 -0800

    Added input to view single or multiple routing on IDE

---
+++ src/main/java/com/NotJustAWeatherApp/API/RouteController/URLConnectionReaderMultiple.java
@@ -0,0 +1,19 @@
+package com.NotJustAWeatherApp.API.RouteController;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class URLConnectionReaderMultiple {
+    public static void main(String[] args) throws Exception {
+        URL oracle = new URL("http://localhost:8080/weather/multiple");
+        URLConnection yc = oracle.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                yc.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
+    }
+}
