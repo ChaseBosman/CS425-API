8a6fd1257d14263f7585def6ed9ce03d9cb29fa3
ChaseBosman <54570221+ChaseBosman@users.noreply.github.com>
2021-04-11 14:17:30 -0700

    Update ResponseBuilder.java
    Added in coordinates related to statistics

--- src/main/java/com/NotJustAWeatherApp/API/RouteWeatherController/ResponseBuilder.java
+++ src/main/java/com/NotJustAWeatherApp/API/RouteWeatherController/ResponseBuilder.java
@@ -1,7 +1,6 @@
 package com.NotJustAWeatherApp.API.RouteWeatherController;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -23,20 +22,59 @@ public class ResponseBuilder {
         {
             JSONObject dayForecast = new JSONObject();
             dayForecast.put("date",getDate(currentDay));
-            dayForecast.put("maxTemp",getMaxTempValue(currentDay));
-            dayForecast.put("minTemp",getMinTempValue(currentDay));
-            dayForecast.put("maxWindSpeed",getWindSpeed(currentDay));
-            dayForecast.put("maxWindGust",getWindGust(currentDay));
-            dayForecast.put("maxHumidity",getMaxHumidity(currentDay));
-            dayForecast.put("minHumidity",getMinHumidity(currentDay));
-            dayForecast.put("maxSnowfallTotal",getMaxSnowfallTotal(currentDay));
-            dayForecast.put("maxQuantitativePrecipitation",getMaxQuantitativePrecipitation(currentDay));
-            dayForecast.put("maxAvgProbabilityOfPrecipitation",getMaxAvgProbabilityOfPrecipitation(currentDay));
-            dayForecast.put("maxProbabilityOfPrecipitation",getMaxProbabilityOfPrecipitation(currentDay));
 
-            double visibility = getMinVisibility(currentDay);
-            if(visibility < Float.MAX_VALUE - 1) {
-                dayForecast.put("minVisibility", visibility);
+            double[] maxTemp = getMaxTempValue(currentDay);
+            dayForecast.put("maxTemp",maxTemp[0]);
+            dayForecast.put("maxTempCoords",maxTemp[1] +"," + maxTemp[2]);
+
+            double[] minTemp = getMinTempValue(currentDay);
+            dayForecast.put("minTemp",minTemp[0]);
+            dayForecast.put("minTempCoords",minTemp[1] +"," + minTemp[2]);
+
+            dayForecast.put("maxWindSpeed",getWindSpeed(currentDay));
+            double[] maxWindSpeed = getWindSpeed(currentDay);
+            dayForecast.put("maxWindSpeed",maxWindSpeed[0]);
+            dayForecast.put("maxWindSpeedCoords",maxWindSpeed[1] +"," + maxWindSpeed[2]);
+
+            double[] maxWindGust = getWindGust(currentDay);
+            dayForecast.put("maxWindGust",maxWindGust[0]);
+            dayForecast.put("maxWindGustCoords",maxWindGust[1]
+                    +"," + maxWindGust[2]);
+
+            dayForecast.put("maxHumidity",getMaxHumidity(currentDay));
+            double[] maxHumidity = getMaxHumidity(currentDay);
+            dayForecast.put("maxHumidity",maxHumidity[0]);
+            dayForecast.put("maxHumidityCoords",maxHumidity[1] +"," + maxHumidity[2]);
+
+            dayForecast.put("minHumidity",getMinHumidity(currentDay));
+            double[] minHumidity = getMinHumidity(currentDay);
+            dayForecast.put("minHumidity",minHumidity[0]);
+            dayForecast.put("minHumidityCoords",minHumidity[1] +"," + minHumidity[2]);
+
+            double[] maxSnowfallTotal = getMaxSnowfallTotal(currentDay);
+            dayForecast.put("maxSnowfallTotal",maxSnowfallTotal[0]);
+            dayForecast.put("maxSnowfallTotalCoords",maxSnowfallTotal[1] +"," + maxSnowfallTotal[2]);
+
+            double[] maxQuantitativePrecipitation = getMaxQuantitativePrecipitation(currentDay);
+            dayForecast.put("maxQuantitativePrecipitation",maxQuantitativePrecipitation[0]);
+            dayForecast.put("maxQuantitativePrecipitationCoords",maxQuantitativePrecipitation[1]
+                    +"," + maxQuantitativePrecipitation[2]);
+
+            double[] maxAvgProbabilityOfPrecipitation = getMaxAvgProbabilityOfPrecipitation(currentDay);
+            dayForecast.put("maxAvgProbabilityOfPrecipitation",maxAvgProbabilityOfPrecipitation[0]);
+            dayForecast.put("maxAvgProbabilityOfPrecipitationCoords",maxAvgProbabilityOfPrecipitation[1]
+                    +"," + maxAvgProbabilityOfPrecipitation[2]);
+
+            double[] maxProbabilityOfPrecipitation = getMaxProbabilityOfPrecipitation(currentDay);
+            dayForecast.put("maxProbabilityOfPrecipitation",maxProbabilityOfPrecipitation[0]);
+            dayForecast.put("maxProbabilityOfPrecipitationCoords",maxProbabilityOfPrecipitation[1]
+                    +"," + maxProbabilityOfPrecipitation[2]);
+
+            double[] visibility = getMinVisibility(currentDay);
+            if(visibility[0] < Float.MAX_VALUE - 1) {
+                dayForecast.put("minVisibility",visibility[0]);
+                dayForecast.put("minVisibilityCoords",visibility[1]
+                        +"," + visibility[2]);
             }
             else
             {
@@ -46,9 +84,11 @@ public class ResponseBuilder {
         }
     }
 
-    double getMaxTempValue(int day)
+    double[] getMaxTempValue(int day)
     {
         float maxVal = -100;
+        String lat = "";
+        String lon = "";
 
         for(JsonNode route: route_weather_responses)
         {
@@ -58,14 +98,21 @@ public class ResponseBuilder {
             {
                 float currVal = Float.parseFloat(data);
                 if (currVal > maxVal)
+                {
                     maxVal = currVal;
+                    lat = route.at("/geometry/coordinates/0/0/1").toPrettyString();
+                    lon = route.at("/geometry/coordinates/0/0/0").toPrettyString();
+                }
             }
         }
-
-        return Math.round(maxVal * 100.0) / 100.0;
+        double[] temp = new double[3];
+        temp[1] = Double.parseDouble(lat);
+        temp[2] = Double.parseDouble(lon);
+        temp[0] = Math.round(maxVal * 100.0) / 100.0;
+        return temp;
     }
 
-    double getMaxAvgProbabilityOfPrecipitation(int day)
+    double[] getMaxAvgProbabilityOfPrecipitation(int day)
     {
         String initial_date = getDate((0));
         int curr_day = Integer.parseInt(initial_date.substring(8,10));
@@ -74,6 +121,8 @@ public class ResponseBuilder {
         String searched_date = DateConverter.addDays(curr_day, curr_month, curr_year, day);
 
         float max_avg = 0;
+        String lat = route_weather_responses.get(0).at("/geometry/coordinates/0/0/1").toPrettyString();
+        String lon = route_weather_responses.get(0).at("/geometry/coordinates/0/0/0").toPrettyString();
 
         for(JsonNode route: route_weather_responses)
         {
@@ -100,12 +149,19 @@ public class ResponseBuilder {
             if(avg > max_avg)
             {
                 max_avg = avg;
+                lat = route.at("/geometry/coordinates/0/0/1").toPrettyString();
+                lon = route.at("/geometry/coordinates/0/0/0").toPrettyString();
             }
         }
-        return Math.round(max_avg * 100.0) / 100.0;
+
+        double[] temp = new double[3];
+        temp[1] = Double.parseDouble(lat);
+        temp[2] = Double.parseDouble(lon);
+        temp[0] = Math.round(max_avg * 100.0) / 100.0;
+        return temp;
     }
 
-    double getMaxProbabilityOfPrecipitation(int day)
+    double[] getMaxProbabilityOfPrecipitation(int day)
     {
         String initial_date = getDate((0));
         int curr_day = Integer.parseInt(initial_date.substring(8,10));
@@ -114,6 +170,8 @@ public class ResponseBuilder {
         String searched_date = DateConverter.addDays(curr_day, curr_month, curr_year, day);
 
         float max_value = 0;
+        String lat = route_weather_responses.get(0).at("/geometry/coordinates/0/0/1").toPrettyString();
+        String lon = route_weather_responses.get(0).at("/geometry/coordinates/0/0/0").toPrettyString();
 
         for(JsonNode route: route_weather_responses)
         {
@@ -137,12 +195,25 @@ public class ResponseBuilder {
             if(value > max_value)
             {
                 max_value = value;
+                lat = route.at("/geometry/coordinates/0/0/1").toPrettyString();
+                lon = route.at("/geometry/coordinates/0/0/0").toPrettyString();
             }
         }
-        return Math.round(max_value * 100.0) / 100.0;
+        if(max_value == 0) {
+            double[] temp = new double[3];
+            temp[1] = 0;
+            temp[2] = 0;
+            temp[0] = 0;
+            return temp;
+        }
+        double[] temp = new double[3];
+        temp[1] = Double.parseDouble(lat);
+        temp[2] = Double.parseDouble(lon);
+        temp[0] = Math.round(max_value * 100.0) / 100.0;
+        return temp;
     }
 
-    double getMaxQuantitativePrecipitation(int day)
+    double[] getMaxQuantitativePrecipitation(int day)
     {
         String initial_date = getDate((0));
         int curr_day = Integer.parseInt(initial_date.substring(8,10));
@@ -151,6 +222,8 @@ public class ResponseBuilder {
         String searched_date = DateConverter.addDays(curr_day, curr_month, curr_year, day);
 
         float max_count = 0;
+        String lat = route_weather_responses.get(0).at("/geometry/coordinates/0/0/1").toPrettyString();
+        String lon = route_weather_responses.get(0).at("/geometry/coordinates/0/0/0").toPrettyString();
 
         for(JsonNode route: route_weather_responses)
         {
@@ -174,12 +247,25 @@ public class ResponseBuilder {
             if(count > max_count)
             {
                 max_count = count;
+                lat = route.at("/geometry/coordinates/0/0/1").toPrettyString();
+                lon = route.at("/geometry/coordinates/0/0/0").toPrettyString();
             }
         }
-        return Math.round(max_count * 100.0) / 100.0;
+        if(max_count == 0) {
+            double[] temp = new double[3];
+            temp[1] = 0;
+            temp[2] = 0;
+            temp[0] = 0;
+            return temp;
+        }
+        double[] temp = new double[3];
+        temp[1] = Double.parseDouble(lat);
+        temp[2] = Double.parseDouble(lon);
+        temp[0] = Math.round(max_count * 100.0) / 100.0;
+        return temp;
     }
 
-    double getMaxSnowfallTotal(int day)
+    double[] getMaxSnowfallTotal(int day)
     {
         String initial_date = getDate((0));
         int curr_day = Integer.parseInt(initial_date.substring(8,10));
@@ -188,6 +274,8 @@ public class ResponseBuilder {
         String searched_date = DateConverter.addDays(curr_day, curr_month, curr_year, day);
 
         float max_count = 0;
+        String lat = route_weather_responses.get(0).at("/geometry/coordinates/0/0/1").toPrettyString();
+        String lon = route_weather_responses.get(0).at("/geometry/coordinates/0/0/0").toPrettyString();
 
         for(JsonNode route: route_weather_responses)
         {
@@ -211,12 +299,25 @@ public class ResponseBuilder {
             if(count > max_count)
             {
                 max_count = count;
+                lat = route.at("/geometry/coordinates/0/0/1").toPrettyString();
+                lon = route.at("/geometry/coordinates/0/0/0").toPrettyString();
             }
         }
-        return Math.round(max_count * 100.0) / 100.0;
+        if(max_count == 0) {
+            double[] temp = new double[3];
+            temp[1] = 0;
+            temp[2] = 0;
+            temp[0] = 0;
+            return temp;
+        }
+        double[] temp = new double[3];
+        temp[1] = Double.parseDouble(lat);
+        temp[2] = Double.parseDouble(lon);
+        temp[0] = Math.round(max_count * 100.0) / 100.0;
+        return temp;
     }
 
-    double getMaxHumidity(int day)
+    double[] getMaxHumidity(int day)
     {
         String initial_date = getDate((0));
         int curr_day = Integer.parseInt(initial_date.substring(8,10));
@@ -225,6 +326,8 @@ public class ResponseBuilder {
         String searched_date = DateConverter.addDays(curr_day, curr_month, curr_year, day);
 
         float maxVal = -100;
+        String lat = route_weather_responses.get(0).at("/geometry/coordinates/0/0/1").toPrettyString();
+        String lon = route_weather_responses.get(0).at("/geometry/coordinates/0/0/0").toPrettyString();
 
         for(JsonNode route: route_weather_responses) {
             String atValue = "/properties/relativeHumidity/values/";
@@ -241,14 +344,22 @@ public class ResponseBuilder {
                 {
                     float currVal = Float.parseFloat(value_response);
                     if (currVal > maxVal)
+                    {
                         maxVal = currVal;
+                        lat = route.at("/geometry/coordinates/0/0/1").toPrettyString();
+                        lon = route.at("/geometry/coordinates/0/0/0").toPrettyString();
+                    }
                 }
             }
         }
-        return Math.round(maxVal * 100.0) / 100.0;
+        double[] temp = new double[3];
+        temp[1] = Double.parseDouble(lat);
+        temp[2] = Double.parseDouble(lon);
+        temp[0] = Math.round(maxVal * 100.0) / 100.0;
+        return temp;
     }
 
-    double getMinHumidity(int day)
+    double[] getMinHumidity(int day)
     {
         String initial_date = getDate((0));
         int curr_day = Integer.parseInt(initial_date.substring(8,10));
@@ -257,6 +368,8 @@ public class ResponseBuilder {
         String searched_date = DateConverter.addDays(curr_day, curr_month, curr_year, day);
 
         float minVal = 150;
+        String lat = route_weather_responses.get(0).at("/geometry/coordinates/0/0/1").toPrettyString();
+        String lon = route_weather_responses.get(0).at("/geometry/coordinates/0/0/0").toPrettyString();
 
         for(JsonNode route: route_weather_responses) {
             String atValue = "/properties/relativeHumidity/values/";
@@ -272,15 +385,22 @@ public class ResponseBuilder {
                 if (!value_response.equals("") && date_response.contains(searched_date))
                 {
                     float currVal = Float.parseFloat(value_response);
-                    if (currVal < minVal)
+                    if (currVal < minVal) {
                         minVal = currVal;
+                        lat = route.at("/geometry/coordinates/0/0/1").toPrettyString();
+                        lon = route.at("/geometry/coordinates/0/0/0").toPrettyString();
+                    }
                 }
             }
         }
-        return Math.round(minVal * 100.0) / 100.0;
+        double[] temp = new double[3];
+        temp[1] = Double.parseDouble(lat);
+        temp[2] = Double.parseDouble(lon);
+        temp[0] = Math.round(minVal * 100.0) / 100.0;
+        return temp;
     }
 
-    double getMinVisibility(int day)
+    double[] getMinVisibility(int day)
     {
         String initial_date = getDate((0));
         int curr_day = Integer.parseInt(initial_date.substring(8,10));
@@ -289,6 +409,8 @@ public class ResponseBuilder {
         String searched_date = DateConverter.addDays(curr_day, curr_month, curr_year, day);
 
         float minVal = Float.MAX_VALUE;
+        String lat = route_weather_responses.get(0).at("/geometry/coordinates/0/0/1").toPrettyString();
+        String lon = route_weather_responses.get(0).at("/geometry/coordinates/0/0/0").toPrettyString();
 
         for(JsonNode route: route_weather_responses) {
             String atValue = "/properties/visibility/values/";
@@ -304,16 +426,27 @@ public class ResponseBuilder {
                 if (!value_response.equals("") && date_response.contains(searched_date))
                 {
                     float currVal = Float.parseFloat(value_response);
-                    if (currVal < minVal)
+                    if (currVal < minVal) {
                         minVal = currVal;
+                        lat = route.at("/geometry/coordinates/0/0/1").toPrettyString();
+                        lon = route.at("/geometry/coordinates/0/0/0").toPrettyString();
+                    }
                 }
             }
         }
 
         if(minVal == Float.MAX_VALUE) {
-            return minVal;
+            double[] temp = new double[3];
+            temp[1] = 0;
+            temp[2] = 0;
+            temp[0] = minVal;
+            return temp;
         }
-        return Math.round(minVal * 100.0) / 100.0;
+        double[] temp = new double[3];
+        temp[1] = Double.parseDouble(lat);
+        temp[2] = Double.parseDouble(lon);
+        temp[0] = Math.round(minVal * 100.0) / 100.0;
+        return temp;
     }
 
     String getDate(int day)
@@ -333,7 +466,7 @@ public class ResponseBuilder {
         return "";
     }
 
-    double getWindSpeed(int day)
+    double[] getWindSpeed(int day)
     {
         String initial_date = getDate((0));
         int curr_day = Integer.parseInt(initial_date.substring(8,10));
@@ -342,6 +475,8 @@ public class ResponseBuilder {
         String searched_date = DateConverter.addDays(curr_day, curr_month, curr_year, day);
 
         float maxVal = -100;
+        String lat = "";
+        String lon = "";
 
         for(JsonNode route: route_weather_responses) {
             String atValue = "/properties/windSpeed/values/";
@@ -357,15 +492,22 @@ public class ResponseBuilder {
                 if (!value_response.equals("") && date_response.contains(searched_date))
                 {
                     float currVal = Float.parseFloat(value_response);
-                    if (currVal > maxVal)
+                    if (currVal > maxVal) {
                         maxVal = currVal;
+                        lat = route.at("/geometry/coordinates/0/0/1").toPrettyString();
+                        lon = route.at("/geometry/coordinates/0/0/0").toPrettyString();
+                    }
                 }
             }
         }
-        return Math.round(maxVal * 100.0) / 100.0;
+        double[] temp = new double[3];
+        temp[1] = Double.parseDouble(lat);
+        temp[2] = Double.parseDouble(lon);
+        temp[0] = Math.round(maxVal * 100.0) / 100.0;
+        return temp;
     }
 
-    double getWindGust(int day)
+    double[] getWindGust(int day)
     {
         String initial_date = getDate((0));
         int curr_day = Integer.parseInt(initial_date.substring(8,10));
@@ -374,6 +516,8 @@ public class ResponseBuilder {
         String searched_date = DateConverter.addDays(curr_day, curr_month, curr_year, day);
 
         float maxVal = -100;
+        String lat = "";
+        String lon = "";
 
         for(JsonNode route: route_weather_responses) {
             String atValue = "/properties/windGust/values/";
@@ -390,16 +534,26 @@ public class ResponseBuilder {
                 {
                     float currVal = Float.parseFloat(value_response);
                     if (currVal > maxVal)
+                    {
                         maxVal = currVal;
+                        lat = route.at("/geometry/coordinates/0/0/1").toPrettyString();
+                        lon = route.at("/geometry/coordinates/0/0/0").toPrettyString();
+                    }
                 }
             }
         }
-        return Math.round(maxVal * 100.0) / 100.0;
+        double[] temp = new double[3];
+        temp[1] = Double.parseDouble(lat);
+        temp[2] = Double.parseDouble(lon);
+        temp[0] = Math.round(maxVal * 100.0) / 100.0;
+        return temp;
     }
 
-    double getMinTempValue(int day)
+    double[] getMinTempValue(int day)
     {
         float minVal = 100;
+        String lat = "";
+        String lon = "";
 
         for(JsonNode route: route_weather_responses)
         {
@@ -410,10 +564,16 @@ public class ResponseBuilder {
                 float currVal = Float.parseFloat(data);
                 if (currVal < minVal)
                     minVal = currVal;
+                    lat = route.at("/geometry/coordinates/0/0/1").toPrettyString();
+                    lon = route.at("/geometry/coordinates/0/0/0").toPrettyString();
             }
         }
 
-        return Math.round(minVal * 100.0) / 100.0;
+        double[] temp = new double[3];
+        temp[1] = Double.parseDouble(lat);
+        temp[2] = Double.parseDouble(lon);
+        temp[0] = Math.round(minVal * 100.0) / 100.0;
+        return temp;
     }
 
     String getResponse(){
